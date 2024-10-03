85125f9dba3bfd4948c38aa60ca1e3d779f2f506
garsidej2005 <GARSIDEJEREMY60@GMAIL.COM>
2024-10-03 11:48:10 +0100

    week2

---
+++ SortingAlgorithms/SortingAlgorithms/MenuSystem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class MenuSystem
+    {
+        string callingClass;
+        Dictionary<string, MethodInfo> menuOptions;
+
+        public MenuSystem(string callingClassNames)
+        {
+            callingClass = callingClassNames;
+            menuOptions = new Dictionary<string, MethodInfo>();
+
+            Type thisType = typeof(Environment);
+            AddMenuItem("ExitApplication", thisType.GetMethod("Exit"));
+        }
+
+        public void RunForever()
+        {
+            while (true)
+            {
+                Run();
+            }
+        }
+
+        public void Run()
+        {
+            DisplayOptions();
+            GetMenuSelection();
+        }
+
+        public void DisplayOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Please choose from the following options: \n");
+            int i = 0;
+
+            foreach (string menuItem in menuOptions.Keys)
+            {
+                Console.WriteLine("(" + i + ") " + menuItem);
+                i++;
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        void GetMenuSelection()
+        {
+            string userInput = Console.ReadLine();
+
+            MethodInfo mi;
+            if (int.TryParse(userInput, out int integerInput))
+            {
+                if (integerInput == 0)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, new object[] { 0 });
+                    Console.ReadKey();
+                }
+
+                else if (integerInput > 0 && integerInput < menuOptions.Count)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, null);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+        public void AddMenuItem(string menuMessage, MethodInfo methodInfo)
+        {
+            menuOptions.Add(menuMessage, methodInfo);
+        }
+
+        public void AddMenuItem(string menuMessage, string methodName, Type callingtype)
+        {
+            MethodInfo methodInfo = callingtype.GetMethod(methodName);
+            menuOptions.Add(menuMessage, methodInfo);
+        }
+
+    }
+
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SortingAlgorithms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("University of Sunderland")]
+[assembly: AssemblyProduct("SortingAlgorithms")]
+[assembly: AssemblyCopyright("Copyright © University of Sunderland 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c1e799b-c7f3-4a7f-9842-3638eb60cf7b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
